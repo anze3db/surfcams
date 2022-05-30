3b127ad3ea4e3717f01ce84ac69e54556913063c
Anže Pečar <anze@pecar.me>
2022-05-30 10:27:19 +0100

    Redner video, cameras from the API

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,215 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:developer';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:video_player/video_player.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() => runApp(const MainWidget());
 
-  // This widget is the root of your application.
+class MainWidget extends StatelessWidget {
+  const MainWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MediaQuery.fromWindow(
+        child: const CupertinoApp(
+            useInheritedMediaQuery: true,
+            title: 'Surf Cams',
+            home: CupertinoPageScaffold(child: SurfCams())));
+  }
+}
+
+class Cams {
+  final String name;
+  final String category;
+  final String url;
+  final String source;
+
+  const Cams({
+    required this.name,
+    required this.category,
+    required this.url,
+    required this.source,
+  });
+
+  factory Cams.fromJson(Map<String, dynamic> json) {
+    return Cams(
+      name: json['name'] as String,
+      category: json['category'] as String,
+      url: json['url'] as String,
+      source: json['source'] as String,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+const camsUrl = 'https://surfcams.pecar.me/api/cams.json';
+Future<List<Cams>> fetchCams() async {
+  final response = await http.get(Uri.parse(camsUrl));
+  if (response.statusCode == 200) {
+    final results = jsonDecode(response.body)['cams'] as List;
+    return results.map((c) => Cams.fromJson(c)).toList();
+  } else {
+    throw Exception('Failed to load');
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class SurfCams extends StatefulWidget {
+  const SurfCams({super.key});
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  // ignore: library_private_types_in_public_api
+  _SurfCamsState createState() => _SurfCamsState();
+}
+
+class LifecycleEventHandler extends WidgetsBindingObserver {
+  final AsyncCallback resumeCallBack;
+  LifecycleEventHandler({required this.resumeCallBack});
+
+  @override
+  Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
+    log("App lifecyle");
+    if (state != AppLifecycleState.resumed) {
+      return;
+    }
+    log("App lifecyle resumed");
+    await resumeCallBack();
+  }
+}
+
+class _SurfCamsState extends State<SurfCams> {
+  late Future<List<Cams>> fetchCamsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCamsFuture = fetchCams();
+    WidgetsBinding.instance.addObserver(LifecycleEventHandler(
+        resumeCallBack: () async => setState(() {
+              log("Fetching after resume");
+              fetchCamsFuture = fetchCams();
+            })));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return CustomScrollView(slivers: <Widget>[
+      const CupertinoSliverNavigationBar(largeTitle: Text('Surf Cams')),
+      SliverFillRemaining(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              FutureBuilder<List<Cams>>(
+                future: fetchCamsFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData && snapshot.data != null) {
+                    return CamsListView(cams: snapshot.data!);
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.error}');
+                  }
+                  // By default, show a loading spinner.
+                  return const CupertinoActivityIndicator();
+                },
+              ),
+              // const SizedBox(height: 120),
+              CupertinoButton.filled(
+                  onPressed: () {
+                    log("Cam view pressed");
+                    // Navigator.push(context,
+                    //     CupertinoPageRoute<Widget>(builder: (BuildContext context) {
+                    //   return const VideoViewPage();
+                    // }));
+                  },
+                  child: const Text('Carcavelos'))
+            ]),
+      )
+    ]);
+  }
+}
+
+class CamsListView extends StatelessWidget {
+  final List<Cams> cams;
+  const CamsListView({Key? key, required this.cams}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final categories = cams.map((c) => c.category).toSet().toList();
+    final categoriesMap = {
+      for (var category in categories)
+        category: cams.where((cam) => cam.category == category)
+    };
+
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: categories.map((category) {
+          return Column(children: <Widget>[
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              category,
+              textAlign: TextAlign.left,
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            // ListView(
+            //     scrollDirection: Axis.horizontal,
+            //     children: const [Text("aaa")]),
+            ...categoriesMap[category]!
+                .map((cam) => Text('${cam.name} ${cam.source}'))
+          ]);
+          ;
+        }).toList());
+  }
+}
+
+class VideoViewPage extends StatelessWidget {
+  final String url;
+  const VideoViewPage({Key? key, required this.url}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VideoView(url: url);
+  }
+}
+
+class VideoView extends StatefulWidget {
+  final String url;
+
+  const VideoView({super.key, required this.url});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _VideoViewState createState() => _VideoViewState();
+}
+
+class _VideoViewState extends State<VideoView> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.url)
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {
+          if (!_controller.value.isPlaying) {
+            _controller.play();
+          }
+        });
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller.value.isInitialized
+        ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          )
+        : Container();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
